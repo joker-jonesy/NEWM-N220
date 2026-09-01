1bcf75337c9b21c54945b516138820a93e3e3d6c
joker-jonesy <spashmcgrath94@yahoo.com>
2026-09-01 16:30:18 -0400

    kermit

---
+++ Week-02/index.js
@@ -0,0 +1,18 @@
+const number = 2;
+let big_bird = 0;
+let user = "Luke";
+
+console.log("This is my const of number: "+number);
+addition(2);
+addition(5);
+addition(10);
+user = 56
+console.log(big_bird);
+console.log(user);
+
+function addition(num){
+    big_bird = big_bird +num;
+}
+
+
+
